738d705494b3737f0d664127252f574bd64a7c10
Moram Navdeep <navadeepmoram2006@gmail.com>
2026-02-02 19:16:04 +0530

    Added Client, Portfolio, and Security JPA entities

---
+++ src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
@@ -0,0 +1,49 @@
+package com.wellsfargo.counselor.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class Portfolio {
+
+    @Id
+    @GeneratedValue
+    private long portfolioId;
+
+    @OneToOne
+    private Client client;
+
+    @Column(nullable = false)
+    private Double totalValue;
+
+    protected Portfolio() {
+    }
+
+    public Portfolio(Client client, Double totalValue) {
+        this.client = client;
+        this.totalValue = totalValue;
+    }
+
+    public Long getPortfolioId() {
+        return portfolioId;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public Double getTotalValue() {
+        return totalValue;
+    }
+
+    public void setTotalValue(Double totalValue) {
+        this.totalValue = totalValue;
+    }
+}
